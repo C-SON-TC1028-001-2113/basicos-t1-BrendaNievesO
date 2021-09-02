9c8ce76489309858b241df5f66eb1d8f7f7f067f
BrendaNievesO <a01252478@itesm.mx>
2021-09-02 15:04:18 +0000

    t1

--- assignments/03Promedio/src/exercise.py
+++ assignments/03Promedio/src/exercise.py
@@ -1,5 +1,14 @@
 def main():
     #escribe tu código abajo de esta línea
+    a = int(input('Calificacion de la primera materia: ')) 
+
+    b = int(input('Calificacion de la segunda materia: ')) 
+
+    c = int(input('Calificacion de la tercera materia: ')) 
+
+    d = int(input('Calificacion de la cuarta materia: ')) 
+    promedio = (a+b+c+d)/4 
+    print ("Promedio del alumno: "+str(promedio))
 
 if __name__ == '__main__':
     main()
